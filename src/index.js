3f3baa8197027446ff59c0ed6a0507b6eb94f298
progbits <53880655+progbits@users.noreply.github.com>
2023-02-19 00:27:39 +0000

    Basic plotting example

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
-import { Femtochart } from './femtochart'
-
-const container = document.getElementById('container')
+import { Femtochart } from './femtochart.js'
 
 let fc = new Femtochart()
-fc.init(container)
+fc.init(document.getElementById('container'))
+
+let series = fc.addSeries('default')
+
+let x = []
+let y = []
+
+let step = 0.001
+let xValue = 0
+for (let i = 0; i <= 10 / step; i++) {
+  xValue = xValue + step
+  x.push(xValue)
+  y.push(Math.sin(xValue) + 5 + Math.random() * 2.5)
+}
+
+series.append(x, y, false)
+fc.draw(false)
